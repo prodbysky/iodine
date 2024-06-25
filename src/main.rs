89d157a6ff4a0884fdda470d6c084210a9207ee0
prodbysky <kyrbynyt@gmail.com>
2024-06-25 13:01:33 +0300

    Time to parse the text!

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the source file to compile
+    #[arg(short)]
+    input: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let source = std::fs::read_to_string(&args.input);
+
+    if source.is_err() {
+        eprintln!("Failed to read input file because: {}", source.unwrap_err());
+        return;
+    }
+    let source = source.unwrap();
+
+    println!("{source}");
+}
