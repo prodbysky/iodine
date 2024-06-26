74d7d81d49f87970467660977050108a516e4d2c
prodbysky <kyrbynyt@gmail.com>
2024-06-26 18:05:13 +0300

    Fixed bugs :)

--- src/lexer.rs
+++ src/lexer.rs
@@ -7,13 +7,15 @@ use std::{
 
 pub struct Lexer<'a> {
     content: Peekable<Chars<'a>>,
+    source: &'a str,
+    pos: usize,
 }
 
 #[derive(Debug)]
-enum Token {
-    Symbol(String),
-    NumericLiteral(String),
-    StringLiteral(String),
+enum Token<'a> {
+    Symbol(&'a str),
+    NumericLiteral(&'a str),
+    StringLiteral(&'a str),
 }
 
 #[derive(Debug)]
@@ -25,10 +27,10 @@ pub enum Operator {
 }
 
 #[derive(Debug)]
-pub enum ILToken {
-    String(String),
+pub enum ILToken<'a> {
+    String(&'a str),
     Number(u64),
-    Symbol(String),
+    Symbol(&'a str),
     Operator(Operator),
 }
 
@@ -51,27 +53,33 @@ impl<'a> Lexer<'a> {
         let string = string.trim();
         Self {
             content: string.chars().peekable(),
+            source: string,
+            pos: 0,
         }
     }
 
     fn trim_whitespace(&mut self) {
         while self.content.peek().is_some_and(|x| x.is_whitespace()) {
             self.content.next();
+            self.pos += 1;
         }
     }
 
-    fn parse_number(&mut self) -> Result<Token, NumberParseError> {
+    fn parse_number(&mut self) -> Result<Token<'a>, NumberParseError> {
         // TODO: Floats, and negative numbers, and maybe hexadecimal values
-        let mut buffer = String::new();
+        let saved_pos = self.pos;
 
         while self.content.peek().is_some_and(|x| !x.is_whitespace()) {
-            buffer.push(self.content.next().unwrap());
+            self.content.next().unwrap();
+            self.pos += 1;
         }
+
+        let buffer = &self.source[saved_pos..self.pos];
 
         for i in 0..buffer.len() {
             let c = buffer.chars().nth(i).unwrap();
             if c.is_alphabetic() || !c.is_ascii_digit() {
-                return Err(NumberParseError(buffer, i));
+                return Err(NumberParseError(buffer.to_string(), i));
             }
         }
 
@@ -79,29 +87,44 @@ impl<'a> Lexer<'a> {
     }
 
     // TODO: Escaped strings
-    fn parse_string(&mut self) -> Token {
-        let mut buffer = String::new();
+    fn parse_string(&mut self) -> Token<'a> {
+        let saved_pos = self.pos;
 
-        while self.content.peek().is_some_and(|&x| x != '"' || x != '\'') {
-            buffer.push(self.content.next().unwrap());
+        // Skip opening quote
+        let opening_quote = self.content.next().unwrap();
+        self.pos += 1;
+
+        while let Some(&current_char) = self.content.peek() {
+            if current_char == opening_quote {
+                break;
+            }
+            self.content.next();
+            self.pos += 1;
         }
 
-        let buffer = buffer.trim_matches('\'').trim_matches('"');
+        // Skip closing quote
+        self.content.next();
+        self.pos += 1;
 
-        Token::NumericLiteral(buffer.to_string())
+        let buffer = &self.source[saved_pos + 1..self.pos - 1];
+
+        Token::StringLiteral(buffer)
     }
 
-    fn parse_symbol(&mut self) -> Token {
-        let mut buffer = String::new();
+    fn parse_symbol(&mut self) -> Token<'a> {
+        let saved_pos = self.pos;
 
         while self.content.peek().is_some_and(|x| !x.is_whitespace()) {
-            buffer.push(self.content.next().unwrap());
+            self.content.next();
+            self.pos += 1;
         }
+
+        let buffer = &self.source[saved_pos..self.pos];
 
         Token::Symbol(buffer)
     }
 
-    fn next_raw(&mut self) -> Option<Token> {
+    fn next_raw(&mut self) -> Option<Token<'a>> {
         self.trim_whitespace();
 
         let current_char = self.content.peek();
@@ -115,7 +138,6 @@ impl<'a> Lexer<'a> {
         if current_char == '"' || current_char == '\'' {
             return Some(self.parse_string());
         }
-
         if current_char.is_ascii_digit() {
             match self.parse_number() {
                 Ok(num) => return Some(num),
@@ -129,8 +151,8 @@ impl<'a> Lexer<'a> {
     }
 }
 
-impl Iterator for Lexer<'_> {
-    type Item = ILToken;
+impl<'a> Iterator for Lexer<'a> {
+    type Item = ILToken<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let raw_token = self.next_raw();
@@ -145,7 +167,7 @@ impl Iterator for Lexer<'_> {
             Token::StringLiteral(str) => Some(ILToken::String(str)),
             Token::NumericLiteral(num) => Some(ILToken::Number(num.parse::<u64>().unwrap())),
             Token::Symbol(name) => {
-                if let Ok(op) = Operator::from_str(name.as_str()) {
+                if let Ok(op) = Operator::from_str(name) {
                     return Some(ILToken::Operator(op));
                 }
 
